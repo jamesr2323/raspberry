63c33c7e12a81ba06941cc3c04816f5cec63c455
James Rees <jarees@gmail.com>
2022-09-26 21:46:57 +0100

    Rhubarb

--- client/src/Player.jsx
+++ client/src/Player.jsx
@@ -1,10 +1,21 @@
 import { useState, useEffect, useRef } from 'react'
 import { startOfDay, add, isWithinInterval } from 'date-fns'
+import { useHotkeys } from 'react-hotkeys-hook'
+
+function say(message) {
+  let utterance = new SpeechSynthesisUtterance(message);
+  speechSynthesis.speak(utterance);
+}
 
 export default function Player() {
   const [auto, setAuto] = useState(false)
   const [mode, setMode] = useState('birds')
   const audioRef = useRef(null)
+
+  useHotkeys('q', () => changeTo('bbc6', "BBC 6 Music"))
+  useHotkeys('w', () => changeTo('nts', "NTS Radio"))
+  useHotkeys('e', () => changeTo('birds', "Birds"))
+  useHotkeys('a', toggleAuto)
 
   useEffect(() => {
     if (!auto) return
@@ -33,16 +44,34 @@ export default function Player() {
     }
   }, [mode])
 
+  function changeTo(mode, message) {
+    say(message)
+    setMode(mode)
+  }
+
+  function toggleAuto() {
+    setAuto(v => {
+      say(`Auto, ${v ? 'off' : 'on'}`)
+      return !v
+    })
+  }
+
   return <div>
     <img src="https://picsum.photos/1200/800" />
+
     { mode === 'birds' && <audio autoPlay ref={audioRef}>
       <source src="http://edge-audio-06-thn.sharp-stream.com/rspb.mp3" />
     </audio> }
     { mode === 'nts' && <audio autoPlay ref={audioRef}>
       <source src="https://stream-relay-geo.ntslive.net/stream" />
     </audio> }
-    <button disabled={auto} onClick={() => setMode('nts')}>Radio</button>
-    <button disabled={auto} onClick={() => setMode('birds')}>Birds</button>
-    <button onClick={() => setAuto(!auto)}>Auto</button>
+    { mode === 'bbc6' && <audio autoPlay ref={audioRef}>
+      <source src="https://stream.live.vc.bbcmedia.co.uk/bbc_6music" />
+    </audio> }
+
+    <button disabled={auto} onClick={() => changeTo('bbc6', 'BBC 6 Music')}>BBC 6</button>
+    <button disabled={auto} onClick={() => changeTo('nts', 'NTS Radio')}>NTS</button>
+    <button disabled={auto} onClick={() => changeTo('birds', 'Birds')}>Birds</button>
+    <button onClick={toggleAuto}>Auto</button>
   </div>
 }

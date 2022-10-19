b24dc0e3825427ee6b4fe401bcbbd66204f9751e
James Rees <jarees@gmail.com>
2022-10-19 23:33:54 +0100

    Bird build

--- client/src/Player.jsx
+++ client/src/Player.jsx
@@ -45,6 +45,42 @@ export default function Player() {
   const [mode, setMode] = useState('birds')
   const audioRef = useRef(null)
   const spotifyRef = useRef(null)
+  const inputRef = useRef(null)
+  const [startHour, setStartHour] = useState(7)
+  const [startMinute, setStartMinute] = useState(0)
+
+  const [fullHour, setFullHour] = useState(7)
+  const [fullMinute, setFullMinute] = useState(30)
+
+  const [settingState, setSettingState] = useState('none')
+  const [value, setValue] = useState('')
+
+  function handleZ() {
+    if (settingState === 'none') {
+      setSettingState('startHour')
+    }
+    setValue('')
+  }
+
+  function handleKey(e) {
+    if (e.key === 'Enter') {
+      if (settingState === 'startHour') {
+        setStartHour(Number(value))
+        setSettingState('startMinute')
+      } else if (settingState === 'startMinute') {
+        setStartMinute(Number(value))
+        setSettingState('fullHour')
+      } else if (settingState === 'fullHour') {
+        setFullHour(Number(value))
+        setSettingState('fullMinute')
+      } else if (settingState === 'fullMinute') {
+        setFullMinute(Number(value))
+        setSettingState('none')
+        say(`Birds ${startHour} ${startMinute}. Radio ${fullHour} ${Number(value)}.`)
+      }
+      setValue('')
+    }
+  }
 
   useHotkeys('q', () => changeTo('bbc6', "BBC 6 Music"))
   useHotkeys('w', () => changeTo('nts', "NTS Radio"))
@@ -52,10 +88,16 @@ export default function Player() {
 
   useHotkeys('o', volumeUp)
   useHotkeys('l', volumeDown)
+  useHotkeys('z', handleZ)
 
   useHotkeys('a', toggleAuto)
 
+
+  console.log({ startHour, startMinute, fullHour, fullMinute })
+
+
   useEffect(() => {
+    serverSpeak('hello')
     getScript("https://sdk.scdn.co/spotify-player.js")
 
     window.onSpotifyWebPlaybackSDKReady = () => {
@@ -72,16 +114,23 @@ export default function Player() {
   }, [])
 
   useEffect(() => {
+    if (inputRef.current) {
+      say(settingState)
+      setTimeout(() => inputRef.current.focus(), 100)
+    }
+  }, [settingState])
+
+  useEffect(() => {
     if (!auto) return
 
     const interval = setInterval(() => {
-      const sevenAm = add(startOfDay(new Date()), { hours: 7, minutes: 0 })
-      const sevenThirtyAm = add(startOfDay(new Date()), { hours: 7, minutes: 30 })
-      const tenAm = add(startOfDay(new Date()), { hours: 10, minutes: 0 })
+      const sevenAm = add(startOfDay(new Date()), { hours: startHour, minutes: startMinute })
+      const sevenThirtyAm = add(startOfDay(new Date()), { hours: fullHour, minutes: fullMinute })
+      const midnight = add(startOfDay(new Date()), { hours: 23, minutes: 59 })
 
       if (isWithinInterval(new Date(), { start: sevenAm, end: sevenThirtyAm })) {
         setMode('birds')
-      } else if (isWithinInterval(new Date(), { start: sevenThirtyAm, end: tenAm })) {
+      } else if (isWithinInterval(new Date(), { start: sevenThirtyAm, end: midnight })) {
         setMode('nts')
       } else {
         setMode('none')
@@ -105,15 +154,20 @@ export default function Player() {
 
   function toggleAuto() {
     setAuto(v => {
-      say(`Auto, ${v ? 'off' : 'on'}`)
       return !v
     })
   }
 
+  useEffect(() => {
+    say(`Auto, ${auto ? 'on' : 'off'}`)
+  }, [auto])
+
   return <div>
-    <Video width="100%" height="100%" controls autoPlay muted loop>
+    { settingState === 'none' && <Video width="100%" height="100%" controls autoPlay muted loop>
       <source src="/video/stormy_short.mp4" type="video/mp4" />
-    </Video>
+    </Video> }
+
+    { settingState !== 'none' && <input type="text" ref={inputRef} value={value} onChange={e => setValue(e.target.value)} onKeyDown={handleKey} /> }
 
     { mode === 'birds' && <audio autoPlay ref={audioRef}>
       <source src="http://edge-audio-06-thn.sharp-stream.com/rspb.mp3" />
